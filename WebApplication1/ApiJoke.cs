7d0eaae45b0aa9680a5f7a487fcfe9acae5a878a
KamilRg <97163663+KamilRg@users.noreply.github.com>
2022-06-30 15:41:40 +0200

    Add files via upload

---
+++ WebApplication1/ApiJoke.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace WebApplication1
+{
+    public class ApiJoke
+    {
+       
+        
+        public string id { get; set; }
+        public string JokeText { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+}
